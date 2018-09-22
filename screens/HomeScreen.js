fa6770c496ab6039d083f6e976201b7758ff1d46
Hamza Ali <hamzali.tas.95@gmail.com>
2018-09-22 03:23:25 +0300

    initial commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {
+  Image,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  Button,
+  View,
+} from 'react-native';
+import { WebBrowser } from 'expo';
+
+import { MonoText } from '../components/StyledText';
+
+export default class HomeScreen extends React.Component {
+  static navigationOptions = {
+    header: null,
+  };
+
+  _handleCreatePress = () => {
+
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.welcomeContainer}>
+          <Image
+            source={
+              __DEV__
+                ? require('../assets/images/robot-dev.png')
+                : require('../assets/images/robot-prod.png')
+            }
+            style={styles.welcomeImage}
+          />
+        </View>
+
+        <Button title={"Create Game!"} onPress={this._handleCreatePress}/>
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    // padding: 10,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: '#fff',
+  },
+  welcomeContainer: {
+    alignItems: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+  },
+  welcomeImage: {
+    width: 100,
+    height: 80,
+    resizeMode: 'contain',
+    marginTop: 3,
+    marginLeft: -10,
+  }
+});
