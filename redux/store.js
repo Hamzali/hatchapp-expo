8cc3f62dfe65d2aec24c9df6eb27fd02895e452d
Yengas <yigitcan@hotmail.com.tr>
2019-02-03 12:14:44 +0300

    removed socket connection from init, will add later.

--- redux/store.js
+++ redux/store.js
@@ -1,21 +1,16 @@
 import { Subject, NEVER } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import epicCreator from './epics/index';
-import socketRegisterCreator from './socket/index';
 import reducer from "./reducers/index";
 import ServerConfig from "../constants/ServerConfig";
-import { Client as ClientCreator, Socket as SocketCreator } from '@hatchapp/client';
+import { Client as ClientCreator } from '@hatchapp/client';
 
 const Client = ClientCreator({ backend: { base_url: ServerConfig.client.base_url } });
-const Socket = SocketCreator({ url: ServerConfig.socket.url });
 
 const { createAnonymous } = Client;
-const { createSocket } = Socket;
 
 const { token$ } = createAnonymous();
-const socket$ = createSocket(token$, 'test');
 const action$ = new Subject();
 const state$ = new Subject();
 
@@ -28,9 +23,7 @@ function stealMiddleware(ra$, rs$){
 // eslint-disable-next-line
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-
-const socketRegister = socketRegisterCreator();
-const epicIndex = epicCreator();
+const epicIndex = epicCreator(token$);
 const epicMiddleware = createEpicMiddleware();
 const rootEpic = combineEpics(epicIndex, stealMiddleware);
 const store = createStore(
@@ -41,7 +34,5 @@ const store = createStore(
 );
 
 epicMiddleware.run(rootEpic);
-store.dispatch({ type: 'hello', payload: 'world' });
-socket$.pipe(switchMap((socket) => socketRegister(socket, action$, state$, store.dispatch))).subscribe(() => null);
 
 export default store;
